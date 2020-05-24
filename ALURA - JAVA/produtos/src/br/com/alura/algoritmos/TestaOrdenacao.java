4975307281d3afda9dc359053dc2d258684b449c
GabrielGodoy01 <61150821+GabrielGodoy01@users.noreply.github.com>
2020-05-24 20:31:35 -0300

    BuscaBinaria terminada

--- ALURA - JAVA/produtos/src/br/com/alura/algoritmos/TestaOrdenacao.java
+++ ALURA - JAVA/produtos/src/br/com/alura/algoritmos/TestaOrdenacao.java
@@ -9,23 +9,59 @@ public class TestaOrdenacao {
                 new Produto("Smart", 46000),
                 new Produto("Fusca", 17000)
         };
+        
+        SelectionSort(produtos, produtos.length);
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        InsertionSort(produtos,produtos.length);
 
-        for (int atual = 0; atual < produtos.length; atual++) {
-            buscaMenor(produtos)
+        for (Produto produto: produtos){
+            System.out.println(produto.getNome() + " custa " + produto.getPreco());
         }
+    }
 
+
+
+    private static void InsertionSort(Produto[] produtos, int quantidadeDeElementos){
+        for(int atual = 1; atual < quantidadeDeElementos; atual++){
+            System.out.println("Estou na casa "+ atual);
+
+            int analise = atual;
+            while(analise>0 && produtos[analise].getPreco() < produtos[analise-1].getPreco()){
+                troca(produtos,analise,analise-1);
+                analise--;
+            }
+        }
+    }
+
+    private static void troca(Produto[] produtos, int primeiro, int segundo) {
+        System.out.println("Estou trocando " + primeiro + " com " + segundo);
+
+        Produto primeiroProduto = produtos[primeiro];
+        Produto segundoProduto = produtos[segundo];
+        System.out.println("Estou trocando " + primeiroProduto.getNome() +
+                " com " + segundoProduto.getNome());
+
+        produtos[primeiro] = segundoProduto;
+        produtos[segundo] = primeiroProduto;
+    }
+
+    private static void SelectionSort(Produto[] produtos, int quantidadeDeElementos) {
+        for (int atual = 0; atual < quantidadeDeElementos; atual++) {
+            System.out.println("Estou na casa " + atual);
+            int menor = buscaMenor(produtos,atual,quantidadeDeElementos-1);
+            troca(produtos, atual, menor);
+        }
     }
 
     private static int buscaMenor(Produto[] produtos, int inicio, int termino) {
-
         int maisBarato = inicio;
-
         for (int atual = inicio; atual <= termino; atual++) {
             if (produtos[atual].getPreco() < produtos[maisBarato].getPreco()) {
                 maisBarato = atual;
             }
         }
-
         return maisBarato;
     }
 }
