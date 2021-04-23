6fbc7f8237ffbdd25c7abd699b5219c3a7bf9e5c
GabrielGodoy01 <gabriel.godoybz@hotmail.com>
2021-04-23 02:57:46 -0300

    nlw_flutter, criação de uma nova tela com widgets novos, Aula 2

--- Flutter/nlw_flutter/lib/core/app_widget.dart
+++ Flutter/nlw_flutter/lib/core/app_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:nlw_flutter/challenge/challenge_page.dart';
 import 'package:nlw_flutter/home/home_page.dart';
 import 'package:nlw_flutter/splash/splash_page.dart';
 
@@ -7,7 +8,7 @@ class AppWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "DevQuiz",
-      home: HomePage(),
+      home: ChallengePage(),
     );
   }
 }
--- Flutter/nlw_flutter/lib/home/home_page.dart
+++ Flutter/nlw_flutter/lib/home/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:nlw_flutter/home/widgets/appbar/appbar_widget.dart';
+import 'package:nlw_flutter/home/widgets/level_button/level_button_widget.dart';
+import 'package:nlw_flutter/home/widgets/quiz_card/quiz_card_widget.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({Key key}) : super(key: key);
@@ -13,6 +15,49 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
        appBar: AppBarWidget(),
+       body: Padding(
+         padding: const EdgeInsets.symmetric(horizontal: 16),
+         child: Column(
+           children: [
+             SizedBox(
+               height: 24,
+             ),
+             Row(
+               mainAxisAlignment: MainAxisAlignment.spaceBetween,
+               children: [
+                LevelButtonWidget(
+                  label: "Fácil",
+                ),
+                LevelButtonWidget(
+                  label: "Médio",
+                ),
+                LevelButtonWidget(
+                  label: "Difícil",
+                ),
+                LevelButtonWidget(
+                  label: "Perito",
+                ),
+              ],
+             ),
+             SizedBox(
+               height: 24,
+             ),
+             Expanded(
+                 child: GridView.count(
+                   crossAxisSpacing: 16,
+                   mainAxisSpacing: 16,
+                   crossAxisCount: 2,
+                 children: [
+                   QuizCardWidget(),
+                   QuizCardWidget(),
+                   QuizCardWidget(),
+                   QuizCardWidget(),
+                 ],
+               ),
+             )
+           ],
+         ),
+       )
     );
   }
 }
